8b15f957fe6c4633092e215482b8632ddd95a33e
Jason Thompson <jason@jthompson.ca>
2013-09-25 06:05:20 -0400

    Initial commit
    -install rails
    -write readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# README
+A blog! This will be my first complete rails app.
+
+My goal is create a simple blog framework with only the features needed 
+to reproduce my personal [wordpress blog](http://www.jthompson.ca):
+
+-blog posts (in markdown)
+-pages (in markdown)
+-tags
+-comments
+-[draft](http://www.draftin.com) integration
+
+The minimum viable product will not include comments.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proust::Application.config.secret_key_base = '7f6f68b784cc2748be0895bdb2ced7801198e5f49443079ead3c0b0c7ddb95d26fec1330de4a49a8c485f07c408f12be2bc7b20b886b6689f3e1040158d5a803'
