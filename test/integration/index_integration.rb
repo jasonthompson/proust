8144f4cd2f2bf5d7e9b745ea27c894624589fc22
Jason Thompson <jason@jthompson.ca>
2013-09-26 06:30:43 -0400

    Add add_entry to Blog
    - Update Readme with reference to Avdi's Objects on Rails
    - Add Post class that stores reference to its blog

--- test/integration/index_integration.rb
+++ test/integration/index_integration.rb
@@ -2,9 +2,14 @@ require_relative '../integration_test_helper'
 
 feature "Viewing blog index" do
 
-  scenario "Viewer should see list of posts" do
+  scenario "Viewer should see the blog title" do
     visit '/'
     page.must_have_content('Test Blog')
+  end
+
+  scenario "Viewer should see list of posts" do
+    visit '/'
+    page.must_have_content('First Post')
   end
   
 end
